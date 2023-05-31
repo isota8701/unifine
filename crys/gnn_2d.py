2f8e4f3e5fb7ca1ef443756de3855d294aef1c49
isota <isota8701@gmail.com>
2023-05-31 10:43:49 +0900

    learn replace x

--- crys/gnn_2d.py
+++ crys/gnn_2d.py
@@ -52,18 +52,17 @@ class Encoder(nn.Module):
                  atom_input_dim: int = cfg.GNN.atom_input_dim,
                  hidden_dim: int= cfg.GNN.hidden_dim,
                  roost_layers: int = cfg.GNN.roost_layers,
-                 concat_dim: int = 64
+                 replace_dim: int = 64
                  ):
         """Initialize class with number of input features, conv layers."""
         super().__init__()
-        self.hidden_features = hidden_dim
-
-        # 1) learn var
+        self.hidden_dim = hidden_dim
+        self.replace_dim = replace_dim
         self.atom_embedding = MLPLayer(atom_input_dim, hidden_dim)
-        self.var_fc = nn.Linear(hidden_dim, hidden_dim)
-        # 2) learn var, concat
-        # self.atom_embedding = MLPLayer(atom_input_dim, hidden_dim - concat_dim)
-        # self.var_fc = nn.Linear(hidden_dim, concat_dim)
+        # 1) learn
+        # self.var_fc = nn.Linear(hidden_dim, hidden_dim)
+        # 2) learn, replace
+        # self.var_fc = nn.Linear(hidden_dim, replace_dim)
         self.module_layers = nn.ModuleList([Roost() for _ in range(roost_layers)])
         self.pooling = AvgPooling()
 
@@ -74,12 +73,22 @@ class Encoder(nn.Module):
         x = fg.ndata.pop("atom_features")
         x = self.atom_embedding(x)
         #################################################
-        # 1) learn var
-        var = self.var_fc(x)
-        x += var*torch.randn_like(x)
-        # 2) learn var, concat (256-concat dim + concat)
-        # var = self.var_fc(x)
-        # x = torch.cat([x, var], dim = 1)
+        # fix
+        x += 0.001*torch.randn_like(x)
+        # fix replace
+        # v = x+ 0.001*torch.randn_like(x)
+        # v = v[:, -64:]
+        # x = torch.cat([x[:, :self.hidden_dim-64], v], dim = 1)
+        # 1) learn
+        # logvar = self.var_fc(x)
+        # std = torch.exp(0.5 * logvar)
+        # x = x +  std*torch.randn_like(std)
+        # 2) learn , replace
+        # logvar = self.var_fc(x)
+        # std = torch.exp(0.5*logvar)
+        # x_a, x_b = x[:, :self.hidden_dim-self.replace_dim], x[:,-self.replace_dim:]
+        # x_b= x_b + std * torch.randn_like(std)
+        # x = torch.cat([x_a, x_b], dim = 1)
         #################################################
         # gated GCN updates: update node, edge features
         for module in self.module_layers:
